ad3fa23e57ecea55d848ee1d95b546a58312e67c
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-05 21:00:06 -0600

    adding files to structure app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 import Landing from "./components/Landing"
 import Login from "./components/Login"
 import Home from "./components/Home"
@@ -8,10 +8,13 @@ function App() {
   return (
     <div className="App">
       <h1>Auth Friends App</h1>
-
-      <Route exact path="/" component={Landing}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/home" component={Home}/>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/home" component={Home}/>
+        <Route component={Login}/>
+      </Switch>
+      
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from "react"
+import useLocalStorage from "../useLocalStorage"
 
 const Login = ()=> {
     return(
