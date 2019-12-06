b1cbd7acf6624fb8563db232ad97998a732a1654
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-05 20:47:15 -0600

    basic routing setup

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Route} from "react-router-dom"
+import Landing from "./components/Landing"
+import Login from "./components/Login"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
       <h1>Auth Friends App</h1>
+
+      <Route exact path="/" component={Landing}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/home" component={Home}/>
     </div>
   );
 }
